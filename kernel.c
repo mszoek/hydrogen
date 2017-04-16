739eb307eb13a3c58b1831a01fd4d85475f59304
Zoë <zoe@pixin.net>
2017-04-15 21:35:51 -0400

    Optimized and fixed printing of startup message

--- kernel.c
+++ kernel.c
@@ -1,28 +1,27 @@
+// H2OS Kernel! CodeGrlz rule.
+
+#define TEXTCOLOR 0x17  // white on blue
+
 void kernel_main(void) {
-  char *msg_startup = "H2OS Kernel v0.0.1 Started!";
+  char msg_startup[] = "H2OS Kernel v0.0.1 Started!", *p=msg_startup;
   char *vidmem = (char*)0xb8000; // Start of video memory
-  unsigned int i = 0;
   unsigned int j = 0;
-  char *p;
 
   // Clear the screen (80x25)
   while (j < 80 * 25 * 2)
   {
-    vidmem[j] = 'X';
-    // Set white on black text
-    vidmem[j+1] = 0xf4;
-    j = j + 2;
+    vidmem[j++] = ' ';
+    vidmem[j++] = TEXTCOLOR;
   }
 
-  p = msg_startup;
-  while (*p != '\0')
+  j=0;
+  do
   {
     // write the char to screen
-    vidmem[i] = *p;
-    ++p;
-    // white on black again
-    vidmem[i+1] = 0x0f;
-    i = i + 2;
+    vidmem[j++] = *p++;
+    vidmem[j++] = TEXTCOLOR;
   }
-  return;
+  while (*p != 0);
+
+  while(1) ;
 }
