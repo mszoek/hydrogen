1cb147511a7713785a073f7bce69f7728eecc391
Alexis Knox <mynameiszxios@gmail.com>
2017-04-15 22:27:14 -0400

    Working on video port driver and cursor position.

--- kernel.c
+++ kernel.c
@@ -1,8 +1,11 @@
 // H2OS Kernel! CodeGrlz rule.
 
-#define TEXTCOLOR 0x17  // white on blue
+#include "drivers/video_ports.h"
+#define TEXTCOLOR 0x0f  // white on black
 
 void kernel_main(void) {
+
+  // Define variables
   char msg_startup[] = "H2OS Kernel v0.0.1 Started!", *p=msg_startup;
   char *vidmem = (char*)0xb8000; // Start of video memory
   unsigned int j = 0;
@@ -14,7 +17,16 @@ void kernel_main(void) {
     vidmem[j++] = TEXTCOLOR;
   }
 
-  j=0;
+  // Set cursor position
+  port_byte_out(0x3d4, 14);
+  int cursor_pos = port_byte_in(0x3d5);
+  cursor_pos = cursor_pos << 8;
+  port_byte_out(0x3d4, 15);
+  cursor_pos += port_byte_in(0x3d5);
+
+  // reset j and draw the text
+
+  j=cursor_pos*2;
   do
   {
     // write the char to screen
