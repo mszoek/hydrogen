9c69267142e1f0bd43526650b85062c5b4bd1176
Zoë Knox <zoe@pixin.net>
2019-03-08 19:07:30 -0500

    First pass at physical memory manager (pmm)

--- kernel/kmem.c
+++ kernel/kmem.c
@@ -2,6 +2,9 @@
  * Zoe Knox 2017-2019
  */
 
+#include "hw/types.h"
+#include "kmem.h"
+
 #define PMM_BLOCKS_PER_BYTE 8
 #define PMM_BLOCK_SIZE 4096
 #define PMM_BLOCK_ALIGN PMM_BLOCK_SIZE
@@ -11,25 +14,43 @@ static UInt32 physUsedBlocks = 0;
 static UInt32 physMaxBlocks = 0;
 static UInt32 *physMemoryMap = 0;
 
-inline void pmmBitmapSet(int bit)
+static inline void pmmBitmapSet(int bit)
 {
   physMemoryMap[bit / 32] |= (1 << (bit % 32));
 }
 
-inline void pmmBitmapUnset(int bit)
+static inline void pmmBitmapUnset(int bit)
 { 
   physMemoryMap[bit / 32] &= ~(1 << (bit % 32));
 }
 
-inline int pmmBitmapTest(int bit)
+static inline int pmmBitmapTest(int bit)
 {
   return physMemoryMap[bit / 32] & (1 << (bit % 32));
+}
+
+/* return memory size in KB */
+UInt32 pmmMemSize()
+{
+  return physMemorySize;
 }
 
 /* return memory size in blocks */
 UInt32 pmmMemSizeBlocks()
 {
   return physMemorySize / PMM_BLOCK_SIZE;
+}
+
+/* return free memory in KB */
+UInt32 pmmMemFree()
+{
+  return (physMemorySize*1024 - (physUsedBlocks*PMM_BLOCK_SIZE)) / 1024;
+}
+
+/* return free memory in blocks */
+UInt32 pmmMemFreeBlocks()
+{
+  return physMaxBlocks - physUsedBlocks;
 }
 
 int pmmFindFirstFree()
@@ -44,77 +65,78 @@ int pmmFindFirstFree()
 			for(j = 0; j < 32; j++)
       {
 				int bit = 1 << j;
-				if (! (_mmngr_memory_map[i] & bit) )
+				if( !(physMemoryMap[i] & bit))
 					return i*4*8+j;
 			}
     }
-  }
- 
+  } 
 	return -1;
 }
 
-void	PMM_init (size_t memSize, physical_addr bitmap) {
+void pmmInit(UInt32 memSize, UInt32 bitmap)
+{
+	physMemorySize = memSize;
+	physMemoryMap = (UInt32*)bitmap;
+	physMaxBlocks = (pmmMemSize()*1024) / PMM_BLOCK_SIZE;
+	physUsedBlocks = pmmMemSizeBlocks();
  
-	_mmngr_memory_size	=	memSize;
-	_mmngr_memory_map	=	(UInt32*) bitmap;
-	_mmngr_max_blocks	=	(PMM_get_memory_size()*1024) / PMM_BLOCK_SIZE;
-	_mmngr_used_blocks	=	PMM_get_block_count();
- 
-	//! By default, all of memory is in use
-	memset (_mmngr_memory_map, 0xf, PMM_get_block_count() / PMM_BLOCKS_PER_BYTE );
+	// By default, all of memory is in use
+	memset((char*)physMemoryMap, 0xf, pmmMemSizeBlocks() / PMM_BLOCKS_PER_BYTE);
 }
 
-void	PMM_init_region (physical_addr base, size_t size) {
- 
+void pmmInitRegion(UInt32 base, UInt32 size)
+{ 
 	int align = base / PMM_BLOCK_SIZE;
 	int blocks = size / PMM_BLOCK_SIZE;
  
-	for (; blocks>0; blocks--) {
-		mmap_unset (align++);
-		_mmngr_used_blocks--;
+	for (; blocks > 0; blocks--)
+  {
+		pmmBitmapUnset(align++);
+		physUsedBlocks--;
 	}
- 
-	mmap_set (0);	//first block is always set. This insures allocs cant be 0
+	pmmBitmapSet(0);	// First block is always set so allocs can't be 0
 }
 
-void	PMM_deinit_region (physical_addr base, size_t size) {
- 
+void pmmDropRegion(UInt32 base, UInt32 size)
+{
 	int align = base / PMM_BLOCK_SIZE;
 	int blocks = size / PMM_BLOCK_SIZE;
  
-	for (; blocks>0; blocks--) {
-		mmap_set (align++);
-		_mmngr_used_blocks++;
+	for(; blocks > 0; blocks--)
+  {
+		pmmBitmapSet(align++);
+		physUsedBlocks++;
 	}
 }
 
-void*	PMM_alloc_block () {
+void *pmmAlloc()
+{
+  int frame;
+  UInt32 addr;
+
+	if(pmmMemFreeBlocks() <= 0)
+		return 0;	// out of memory!
  
-	if (PMM_get_free_block_count() <= 0)
-		return 0;	//out of memory
+	frame = pmmFindFirstFree();
+ 	if (frame == -1)
+		return 0;	// out of memory!
  
-	int frame = mmap_first_free ();
- 
-	if (frame == -1)
-		return 0;	//out of memory
- 
-	mmap_set (frame);
- 
-	physical_addr addr = frame * PMM_BLOCK_SIZE;
-	_mmngr_used_blocks++;
+	pmmBitmapSet(frame);
+	addr = frame * PMM_BLOCK_SIZE;
+	physUsedBlocks++;
  
 	return (void*)addr;
 }
 
-    void	PMM_free_block (void* p) {
-     
-    	physical_addr addr = (physical_addr)p;
-    	int frame = addr / PMM_BLOCK_SIZE;
-     
-    	mmap_unset (frame);
-     
-    	_mmngr_used_blocks--;
-    }
+void pmmFree(void *p)
+{
+  UInt32 addr = (UInt32)p;
+  int frame = addr / PMM_BLOCK_SIZE;
+
+  pmmBitmapUnset(frame);
+  physUsedBlocks--;
+}
+
 
 void memcpy(char *dst, char *src, unsigned int len)
 {
