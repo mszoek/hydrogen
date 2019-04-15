dbd41ffbb44a727d7de1c8473ebd748620aac8c5
Alexis Knox <mynameiszxios@gmail.com>
2019-04-15 09:25:00 -0400

    Began work on hylibc for userspace programs

---
+++ userspace/hylibc/includes/types.h
@@ -0,0 +1,34 @@
+#ifndef HY_TYPES_H
+#define HY_TYPES_H
+
+/* Integers */
+
+typedef unsigned long long UInt64;
+typedef unsigned       int UInt32;
+typedef unsigned     short UInt16;
+typedef unsigned      char UInt8;
+typedef signed   long long SInt64;
+typedef signed         int SInt32;
+typedef signed       short SInt16;
+typedef signed        char SInt8;
+
+/*
+	char, short, int, long, and long long should
+	all be included by default during compilation.
+	If not, FIXME: implement char, short, int, etc...
+*/
+
+/* Storage sizes */
+
+#define KB 1024
+#define MB (KB * 1024)
+#define GB (MB * 1024)
+#define TB (GB * 1024)
+#define PB (TB * 1024)
+
+/* C types */
+typedef UInt64 size_t
+typedef UInt64 time_t // TODO: Determine if time_t is actually a UInt64 or not...
+
+
+#endif
