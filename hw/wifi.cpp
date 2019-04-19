7354feaaa33781ef41a130e7d2aec9c3330e30cd
Alexis Knox <mynameiszxios@gmail.com>
2019-04-18 22:06:48 -0400

    Beginnings of a wifi driver

---
+++ hw/wifi.cpp
@@ -0,0 +1,16 @@
+/*
+ * H2 Wireless Networking Driver
+ * Copyright (C) 2017-2019 Zoe & Alexis Knox. All rights reserved.
+ */
+
+#include <hw/wifi.h>
+
+WirelessController::WirelessController()
+{
+	
+}
+
+WirelessController::~WirelessController()
+{
+
+}
